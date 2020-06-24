cc4f216c15ca64da800af6bc1b1ec54b61c10cce
Gao-Yuying <gyy4real@hotmail.com>
2020-06-24 13:56:08 +0800

    Update config.

--- Music Advisor/task/src/advisor/Config.java
+++ Music Advisor/task/src/advisor/Config.java
@@ -10,8 +10,8 @@ public class Config {
     public static final String featuredPath = "/v1/browse/featured-playlists";
     public static final String newPath = "/v1/browse/new-releases";
     public static final String categoriesPath = "/v1/browse/categories";
-    public static final String clientId = "cdff646623a74434a414e619c8d1a807";
-    public static final String clientSecret = "91a74a44bd6f455d94270a8ad5fb92c2";
+    public static final String clientId = "your client ID";
+    public static final String clientSecret = "your client secret";
 
     public Config(String accessPath, String resourcePath, String pageStr) {
         Config.accessPath = accessPath == null ? "https://accounts.spotify.com" : accessPath;
